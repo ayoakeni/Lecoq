28a28a0f175d20a64465f7bd7f7ee08f4f16eebe
ayo akeni <ayoakeni64@gmail.com>
2025-01-13 11:10:11 +0100

    update

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -11,6 +11,7 @@ const Blog = ({  }) => {
       image: Opportunities,
       author: "Busayo Akinjagunla",
       date: "Jan 10, 2025",
+      views: "500",
       title: "5 Sure-banker reasons why you need French",
       excerpt: "In our increasingly interconnected world, learning a new language can open doors to countless opportunities. Among the many languages you could choose to study, French stands out as a particularly valuable option. Here are five compelling reasons why learning French is important.",
       link: "/blog-post-1",
@@ -19,6 +20,7 @@ const Blog = ({  }) => {
       image: Aceexam,
       author: "Busayo Akinjagunla",
       date: "Jan 12, 2025",
+      views: "700",
       title: "Unlocking Migration Opportunities",
       excerpt: "Learning French is not just about mastering a language; it’s about unlocking new opportunities, enriching your cultural experiences, and enhancing your cognitive abilities. Whether you’re looking to advance your career, travel the world, or simply appreciate the beauty of a new language, French offers countless benefits that make it a worthwhile endeavor. So, why not take the plunge and start your journey into the French language today?",
       link: "/blog-post-2",
@@ -27,6 +29,7 @@ const Blog = ({  }) => {
       image: Gplay,
       author: "Busayo Akinjagunla",
       date: "Jan 14, 2025",
+      views: "500",
       title: "5 Sure-banker reasons why you need French",
       excerpt: "In our increasingly interconnected world, learning a new language can open doors to countless opportunities. Among the many languages you could choose to study, French stands out as a particularly valuable option. Here are five compelling reasons why learning French is important.",
       link: "/blog-post-3",
@@ -35,6 +38,7 @@ const Blog = ({  }) => {
       image: Gcontent,
       author: "Busayo Akinjagunla",
       date: "Jan 16, 2025",
+      views: "3000",
       title: "Unlocking Migration Opportunities",
       excerpt: "Learning French is not just about mastering a language; it’s about unlocking new opportunities, enriching your cultural experiences, and enhancing your cognitive abilities. Whether you’re looking to advance your career, travel the world, or simply appreciate the beauty of a new language, French offers countless benefits that make it a worthwhile endeavor. So, why not take the plunge and start your journey into the French language today?",
       link: "/blog-post-4",
@@ -47,10 +51,16 @@ const Blog = ({  }) => {
         <div className="blogPost" key={index}>
           <img src={blog.image} alt={blog.title} className="blogImage" />
           <div className="blogDetails">
-            <ul className="blogMeta">
-              <li className="blogAuthor">{blog.author}</li>
-              <li className="blogDate">{blog.date}</li>
-            </ul>
+            <div className="blogMeta">
+              <li className="nameDate">
+                <span>{blog.author}</span>
+                <i class="fa-solid fa-asterisk"></i>
+                <span>{blog.date}</span>
+              </li>
+              <span className="views">
+                <i className="fa-solid fa-eye"></i>{blog.views}
+              </span>
+            </div>
             <strong className="blogTitle">{blog.title}</strong>
             <p className="blogExcerpt">{blog.excerpt}</p>
             <button
