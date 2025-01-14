fa142e0e15d2c4bb022f35abbeec8bd171a726fc
ayo akeni <ayoakeni64@gmail.com>
2025-01-14 18:47:21 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import Navbar from "./components/navbar";
 import LandingPage from "./pages/LandingPage";
 import Blog from "./pages/Blog";
@@ -11,7 +11,18 @@ import ScrollToTopButton from "./components/scrotoTop";
 import ContactUs from "./components/contact";
 import "./App.css";
 
-import img from "./assets/images/mad-designer.png"
+import img from "./assets/images/mad-designer.png";
+
+// NormalizePath component to clean up URLs with double slashes
+const NormalizePath = ({ children }) => {
+  const location = useLocation();
+  const normalizedPath = location.pathname.replace(/\/{2,}/g, "/");
+
+  if (normalizedPath !== location.pathname) {
+    return <Navigate to={normalizedPath} replace />;
+  }
+  return children;
+};
 
 function App() {
   const contactRef = useRef(null);
@@ -20,19 +31,26 @@ function App() {
   return (
     <div>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/blogs" element={<Blog />} />
-        <Route path="/blog/:id" element={<BlogDetails />} />
-        <Route path="/educational-service" element={<Educational />} />
-        <Route path="/non-educational-service" element={<NonEducational />} />
-        <Route path="*" element={
-          <div className="errorPage">
-            <span>Page not found :<strong>404</strong>check the address again</span>
-            <img src={img} alt="error image" />
-          </div>} 
-        />
-      </Routes>
+      <NormalizePath>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/blogs" element={<Blog />} />
+          <Route path="/blog/:id" element={<BlogDetails />} />
+          <Route path="/educational-service" element={<Educational />} />
+          <Route path="/non-educational-service" element={<NonEducational />} />
+          <Route
+            path="*"
+            element={
+              <div className="errorPage">
+                <span>
+                  Page not found :<strong>404</strong> Check the address again
+                </span>
+                <img src={img} alt="error" />
+              </div>
+            }
+          />
+        </Routes>
+      </NormalizePath>
       <WhatsappChatBox />
       <ScrollToTopButton />
       <section ref={contactRef} id="contact">
